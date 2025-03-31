531964d58f1e0b8b37698cb6142983dab2db81e1
yesmine1343 <bellakhalyesmine@gmail.com>
2025-03-31 16:58:47 +0100

    initial commit

---
+++ login/welcome.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+<form action="welcome.php" method="post">
+    <table class="table" class="table-dark" class="mt-3 mb-3" align="center">
+    <tr>
+    <th colspan="2"><h2 align="center">Login</h2></th>
+    </tr>
+    <tr>
+    <td>Username:</td>
+    <td><input type="text" name="uname"></td>
+    </tr>
+    <tr>
+    <td>Password:</td>
+    <td><input type="password" name="pwd"></td>
+    </tr>
+    <tr>
+    <td align="right" colspan="2"><input type="submit" name="login" value="login"></td>
+    </tr>
+</body>
